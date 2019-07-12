a5cbc064fd37e4846c3fe21edc555ea550d7ebd2
Rob Winch <rwinch@users.noreply.github.com>
2019-07-12 11:41:09 -0500

    saving

--- flight-tracker/src/main/java/io/spring/sample/flighttracker/FlightTrackerApplication.java
+++ flight-tracker/src/main/java/io/spring/sample/flighttracker/FlightTrackerApplication.java
@@ -1,7 +1,10 @@
 package io.spring.sample.flighttracker;
 
+import io.rsocket.RSocketFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.rsocket.server.ServerRSocketFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FlightTrackerApplication {
@@ -9,5 +12,4 @@ public class FlightTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FlightTrackerApplication.class, args);
 	}
-
 }
--- radar-collector/src/main/java/io/spring/sample/radarcollector/RadarCollectorApplication.java
+++ radar-collector/src/main/java/io/spring/sample/radarcollector/RadarCollectorApplication.java
@@ -1,7 +1,32 @@
 package io.spring.sample.radarcollector;
 
+import io.rsocket.Payload;
+import io.rsocket.RSocketFactory;
+import io.rsocket.plugins.RSocketInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.rsocket.server.ServerRSocketFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.rsocket.annotation.support.MetadataExtractor;
+import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.MimeType;
+import reactor.core.publisher.Mono;
+import rsocket.interceptor.PayloadChain;
+import rsocket.interceptor.PayloadInterceptor;
+import rsocket.interceptor.PayloadRSocketInterceptor;
+import security.AuthenticationPayloadInterceptor;
+import security.AuthorizationPayloadInterceptor;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasRole;
 
 @SpringBootApplication
 public class RadarCollectorApplication {
@@ -10,4 +35,51 @@ public class RadarCollectorApplication {
 		SpringApplication.run(RadarCollectorApplication.class, args);
 	}
 
+	@Bean
+	ServerRSocketFactoryCustomizer securityCustomizer(RSocketMessageHandler handler) {
+		return new ServerRSocketFactoryCustomizer() {
+
+			@Override
+			public RSocketFactory.ServerRSocketFactory apply(
+					RSocketFactory.ServerRSocketFactory serverRSocketFactory) {
+				return serverRSocketFactory.addResponderPlugin(security(handler.getMetadataExtractor()));
+			}
+		};
+	}
+
+	private static RSocketInterceptor security(MetadataExtractor extractor) {
+		UserDetails rob = User.withDefaultPasswordEncoder()
+				.username("rob")
+				.password("password")
+				.roles("USER", "ADMIN")
+				.build();
+		UserDetails rossen = User.withDefaultPasswordEncoder()
+				.username("rossen")
+				.password("password")
+				.roles("USER")
+				.build();
+		MapReactiveUserDetailsService uds = new MapReactiveUserDetailsService(
+				rob, rossen);
+		ReactiveAuthenticationManager manager = new UserDetailsRepositoryReactiveAuthenticationManager(uds);
+		List<PayloadInterceptor> payloadInterceptors = Arrays
+				.asList(new LoggingPayloadInterceptor(extractor), new AuthenticationPayloadInterceptor(manager), new AuthorizationPayloadInterceptor(hasRole("ADMIN")));
+
+		return new PayloadRSocketInterceptor(payloadInterceptors);
+	}
+
+	static class LoggingPayloadInterceptor implements PayloadInterceptor {
+		private final MetadataExtractor extractor;
+
+		LoggingPayloadInterceptor(MetadataExtractor extractor) {
+			this.extractor = extractor;
+		}
+
+		@Override
+		public Mono<Void> intercept(Payload payload, PayloadChain chain) {
+			Map<String, Object> headers = this.extractor
+					.extract(payload, new MimeType("message", "x.rsocket.routing.v0"));
+			System.out.println(headers.get(MetadataExtractor.ROUTE_KEY));
+			return chain.next(payload);
+		}
+	}
 }
