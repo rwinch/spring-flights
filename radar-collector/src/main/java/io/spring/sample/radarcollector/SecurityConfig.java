dc49a49e236899245f00e4090db7b52e6b5c5a21
Rob Winch <rwinch@users.noreply.github.com>
2019-08-14 16:45:06 -0500

    Use PayloadSocketAcceptorInterceptor

--- radar-collector/src/main/java/io/spring/sample/radarcollector/SecurityConfig.java
+++ radar-collector/src/main/java/io/spring/sample/radarcollector/SecurityConfig.java
@@ -1,14 +1,18 @@
 package io.spring.sample.radarcollector;
 
+import io.rsocket.SocketAcceptor;
+import org.springframework.boot.rsocket.server.RSocketServerBootstrap;
+import org.springframework.boot.rsocket.server.RSocketServerFactory;
 import org.springframework.boot.rsocket.server.ServerRSocketFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
 import org.springframework.security.config.annotation.rsocket.EnableRSocketSecurity;
 import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.rsocket.interceptor.PayloadRSocketInterceptor;
+import org.springframework.security.rsocket.interceptor.PayloadSocketAcceptorInterceptor;
 
 /**
  * @author Rob Winch
@@ -33,7 +37,7 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	PayloadRSocketInterceptor rsocketInterceptor(RSocketSecurity rsocket) {
+	PayloadSocketAcceptorInterceptor rsocketInterceptor(RSocketSecurity rsocket) {
 		rsocket
 			.authorizePayload(authorize -> {
 				authorize
@@ -43,8 +47,27 @@ public class SecurityConfig {
 		return rsocket.build();
 	}
 
+
 	@Bean
-	ServerRSocketFactoryCustomizer securityCustomizer(PayloadRSocketInterceptor rsocketInterceptor) {
-		return serverRSocketFactory -> serverRSocketFactory.addResponderPlugin(rsocketInterceptor);
+	RSocketServerBootstrap rSocketServerBootstrap(
+			RSocketServerFactory rSocketServerFactory,
+			RSocketMessageHandler rSocketMessageHandler,
+			PayloadSocketAcceptorInterceptor rsocketInterceptor) {
+		SocketAcceptor delegate = rSocketMessageHandler.serverResponder();
+		SocketAcceptor securityAcceptor = rsocketInterceptor.apply(delegate);
+		return new RSocketServerBootstrap(rSocketServerFactory,
+				securityAcceptor);
+	}
+
+//	@Bean
+	// FIXME: Does not work because NettyRSocketServerFactory:124 sets acceptor after ServerRSocketFactoryCustomizer are applied
+	ServerRSocketFactoryCustomizer securityCustomizer(PayloadSocketAcceptorInterceptor security,
+			RSocketMessageHandler rSocketMessageHandler) {
+		return serverRSocketFactory -> {
+			SocketAcceptor delegate = rSocketMessageHandler.serverResponder();
+			SocketAcceptor securityAcceptor = security.apply(delegate);
+			serverRSocketFactory.acceptor(securityAcceptor);
+			return serverRSocketFactory;
+		};
 	}
 }
